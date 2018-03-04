88c97a2116ff2b05be512d854f258fe9eaa9ddce
LeonardoAvendaño <leavendanoro@unal.edu.co>
2018-03-04 11:40:41 -0500

    Se modifican los componentes Board.js y Square.js; ahora Board le da el estado a los quares.

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import Square from './Square.js';
 
-class Board extends React.Component {
+class Board extends React.Component{
 
-  /*Definicion del método para renderizar un cuadrado
+  constructor(props){
+      super(props);
+      this.state = {
+          squares: Array(9).fill(null),
+      };
+  }
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+    /*Definicion del método para renderizar un cuadrado
 
   */
   renderSquare(i) {
-    return <Square value={i}/>;
+    return <Square value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)}/>;
   }
 
   render() {
