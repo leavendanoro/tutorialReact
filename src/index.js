9e381c261d644951df3884f8509911cb41cc42f0
LeonardoAvendaño <leavendanoro@unal.edu.co>
2018-03-04 11:15:06 -0500

    Se crea la estructura de carpetas de src/, y se añade el esqueleto de los componentes.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import Game from './components/Game.js';
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,  
+  document.getElementById('root')
+);
